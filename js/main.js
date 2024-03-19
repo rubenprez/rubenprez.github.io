32a1979a17fb9005de2e975d56909ad33cef0762
rubenprez <rubenprez@mail.com>
2024-03-18 22:30:30 -0700

    this is my last checkpoint of save website [backup]

--- js/main.js
+++ js/main.js
@@ -1,1 +1,68 @@
-//http://i.giphy.com/8SylEdgXn0BIQ.gif
+// JavaScript for audio waveform visualizer
+window.onload = function() {
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioContext = new AudioContext();
+  const canvas = document.getElementById('waveform');
+  const canvasContext = canvas.getContext('2d');
+
+  function drawWaveform(analyser) {
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasContext.fillStyle = 'rgba(255, 255, 255, 0.1)';
+    canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasContext.lineWidth = 2;
+    canvasContext.strokeStyle = 'rgb(103, 177, 246)';
+    canvasContext.beginPath();
+
+    const sliceWidth = WIDTH * 1.0 / bufferLength;
+    let x = 0;
+
+    for(let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasContext.moveTo(x, y);
+      } else {
+        canvasContext.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasContext.lineTo(canvas.width, canvas.height/2);
+    canvasContext.stroke();
+  }
+
+  // Create an analyser node for each audio element
+  const audioElements = document.querySelectorAll('audio');
+  const analysers = [];
+
+  audioElements.forEach(audio => {
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 256;
+
+    const audioSource = audioContext.createMediaElementSource(audio);
+    audioSource.connect(analyser);
+    audioSource.connect(audioContext.destination);
+
+    analysers.push(analyser);
+  });
+
+  // Start drawing the waveform for each analyser
+  function updateWaveform() {
+    analysers.forEach(analyser => {
+      drawWaveform(analyser);
+    });
+    requestAnimationFrame(updateWaveform);
+  }
+  
+  updateWaveform();
+};
