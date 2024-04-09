199459a78728ed7f499140a753dacdfbf1c72d72
rubenprez <rubenprez@mail.com>
2024-04-09 12:41:41 -0700

    latest n greatest bullshit

--- js/main.js
+++ js/main.js
@@ -66,3 +66,32 @@ window.onload = function() {
   
   updateWaveform();
 };
+
+
+
+let currentPhotoIndex = 0;
+const photos = document.querySelectorAll('.photo');
+
+function showPhoto(index) {
+    photos.forEach((photo, idx) => {
+        if (idx === index) {
+            photo.classList.add('active');
+        } else {
+            photo.classList.remove('active');
+        }
+    });
+}
+
+function nextPhoto() {
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+    showPhoto(currentPhotoIndex);
+}
+
+function prevPhoto() {
+    currentPhotoIndex = (currentPhotoIndex - 1 + photos.length) % photos.length;
+    showPhoto(currentPhotoIndex);
+}
+
+// Show the first photo initially
+showPhoto(currentPhotoIndex);
+
